98c991effc79b999d6f9d89b03f86d7969dccb1b
Edward <llanca872@gmail.com>
2018-04-04 16:54:30 -0500

    add card chatbot-edward

--- index.js
+++ index.js
@@ -15,13 +15,46 @@ app.post('/api/messages', connector.listen());
 app.listen(process.env.PORT, () => {
     console.log(`app its works in port ${process.env.PORT}!!!!`)
 });
-const bot = new builder.UniversalBot(connector, (session) => {
-    session.send('habla barrio');
+const bot = new builder.UniversalBot(connector,[
+(session) => {
+    session.send('hola');
     session.beginDialog('startDialog');
-})
+
+}]);
 
 bot.dialog('startDialog', [
     (session, args, next) => {
-        builder.Prompts.text(session, "como te shamas?");
+        builder.Prompts.text(session, 'Cual es tu nombre?');
+    },
+    (session, arg, next) => {
+        builder.Prompts.text(session, 'estas libre para continuar?');
+        console.log(next)
+    },
+    (session, arg, next) => {
+        builder.Prompts.text(session, 'deseas conocer informacion de tu vehiculo?');
+    },
+    (session, results) => {
+        var selectedCardName = results.response.entity;
+        var card = new builder.HeroCard(session)
+        .title('card info vehiculo')
+        .subtitle('descripcion ')
+        .text('contenido chatbot Edward')
+        .images([
+            builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
+        ])
+        .buttons([
+            builder.CardAction.openUrl(session, 'https://docs.microsoft.com/bot-framework/', 'Get Started')
+        ]);
+
+        var msg = new builder.Message(session).addAttachment(card);
+        session.send(msg);
+        session.replaceDialog('second');
+}
+])
+
+bot.dialog('second', [
+    (session, arg, next) => {
+        builder.Prompts.text(session, 'Cual es tu edad?');
+       // session.endDialog('Chau')
     }
 ])
